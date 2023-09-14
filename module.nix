d17a7d3108d9073eaeefddc62b6e2d29c9850a4f
Calum MacRae <hi@cmacr.ae>
2023-09-14 15:58:51 +0100

    feature: configurable/extensible tasks
    These changes introduce the `lollypops.tasks` & `lollypops.extraTasks` options
    to the module. See the associated change to the README to get an idea of its use
    case :)

--- module.nix
+++ module.nix
@@ -145,6 +145,44 @@ in
         };
       };
     };
+
+    tasks = mkOption {
+      type = types.listOf types.str;
+      default = [ "deploy-flake" "deploy-secrets" "rebuild" ];
+      description = "The list of tasks to run for each host.";
+    };
+
+    extraTasks = mkOption {
+      type = types.attrsOf (types.submodule {
+        options = {
+          dir = mkOption {
+            type = types.either types.path types.str;
+            default = ".";
+            description = "Directory in which the task should run.";
+          };
+          deps = mkOption {
+            type = types.listOf types.str;
+            default = [ ];
+            description = "Dependencies for the task.";
+          };
+          desc = mkOption {
+            type = types.str;
+            description = "Description for the task.";
+          };
+          cmds = mkOption {
+            type = types.listOf types.str;
+            description = "Commands for the task.";
+          };
+        };
+      });
+      default = { };
+      description = ''
+        Extra tasks to run for the host. If any are defined with the same name as the default tasks
+        (<literal>deploy-secrets</literal>, <literal>rebuild</literal>, <literal>deploy-flake</literal>)
+        the original tasks will be overriden.
+      '';
+
+    };
   };
 
   config = {
